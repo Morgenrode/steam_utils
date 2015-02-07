a4de6f3c5b5b3088f63fb94456e6e7fbaaca91d1
Morgenrode <dsg14@zips.uakron.edu>
2015-02-07 04:53:05 -0600

    fixed a bunch of stupid stuff I wrote

--- check_wear.py
+++ check_wear.py
@@ -1,4 +1,4 @@
-'''This is a poorly written script for spitting out quality data for 
+'''This is a poorly written script for spitting out quality data from 
 CS:GO weapons for trading purposes. Functionality may be added as I 
 learn what I'm doing'''
 
@@ -12,13 +12,13 @@ try:
 except:
     print('''\n
     -----USAGE-----\n
-    python3 check_wear.py  steam_item_url\n
+    python3 check_wear.py steam_item_url\n
     ---------------''')
 
 HEADERS = {'User-Agent': 'Mozilla/5.0'} # spoof useragent
 STEAM_API_KEY = 'BD30CBB6AEDB0F64B7A879174B115CDA' #from dev api key
 
-def get_steamid64(url):
+def get_steamid64(target_url):
     '''Gets steam_id64 either from the url directly, or from xml if the user has
     set a vanity url'''
 
@@ -37,7 +37,7 @@ def get_steamid64(url):
             except ValueError as v:
                 print(v, '\nInvalid steamcommunity inventory url')
     except:
-        print('Imporperly formed url')
+        print('Improperly formed url')
 
     req = Request('http://steamcommunity.com/id/{0}/?xml=1'.format(vanity_name), headers = HEADERS)
     steamxml = etree.parse(urlopen(req))
@@ -47,16 +47,26 @@ def get_steamid64(url):
     except:
         pass
 
-def parse(page):
-    #parse out the wear value
-    pass
 
-def get_inventory(api=STEAM_API_KEY, user_id='76561198020353514'):
+def get_inventory(api_key, user_id):
     '''Download the specified user's CS:GO inventory page using urllib'''
 
-    req = Request('http://api.steampowered.com/IEconItems_730/GetPlayerItems/v0001/?key={0}&SteamID={1}&format=xml'.format(steam_api_key, 
+    req = Request('http://api.steampowered.com/IEconItems_730/GetPlayerItems/v0001/?key={0}&SteamID={1}&format=xml'.format(STEAM_API_KEY, 
     user_id), headers = HEADERS)
     webpage = etree.parse(urlopen(req))
+    return webpage
+    
+def parse(url):
+    steam_id64, item_id = get_steamid64(url)
+    page = get_inventory(api_key=STEAM_API_KEY, user_id=steam_id64).getroot()
+    for child in page.findall('./items/'):
+        if child.find('id').text == item_id:
+            print('Found it') # gettin' real close boys
+
+
 
         
-get_steamid64(url) 
+# ------------------ #
+if __name__ == '__main__':
+    print('Fetching item wear data...')
+    parse(url)
